2386cfd239bd2dee910639ffdcd9649d2df842bb
VINATOR2000\vinaz <vina.zahnd@gmail.com>
2023-09-28 11:08:18 +0200

    added test refactoring DoMagic

---
+++ clang-tools-extra/clangd/refactor/tweaks/DoMagic.cpp
@@ -0,0 +1,50 @@
+//===--- SwapIfBranches.cpp --------------------------------------*- C++-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#include "ParsedAST.h"
+#include "SourceCode.h"
+#include "refactor/Tweak.h"
+#include "support/Logger.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/Stmt.h"
+#include "clang/Basic/LangOptions.h"
+#include "clang/Basic/SourceLocation.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Tooling/Core/Replacement.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Error.h"
+
+namespace clang {
+namespace clangd {
+namespace {
+/// Test tweak for Vina
+class DoMagic : public Tweak {
+public:
+  const char *id() const final;
+
+  bool prepare(const Selection &Inputs) override;
+  Expected<Effect> apply(const Selection &Inputs) override;
+  std::string title() const override { return "Do magic stuff"; }
+  llvm::StringLiteral kind() const override {
+    return CodeAction::REFACTOR_KIND;
+  }
+};
+
+REGISTER_TWEAK(DoMagic)
+
+bool DoMagic::prepare(const Selection &Inputs) {
+  return true;
+}
+
+Expected<Tweak::Effect> DoMagic::apply(const Selection &Inputs) {
+  return Effect::showMessage("Magic happened!! \\(*0*)/");
+}
+
+} // namespace
+} // namespace clangd
+} // namespace clang
