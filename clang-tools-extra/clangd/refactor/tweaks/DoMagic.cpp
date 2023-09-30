b43b18b4bc6ffd1639976950957caaf6364982aa
VINATOR2000\vinaz <vina.zahnd@gmail.com>
2023-09-30 14:33:25 +0200

    tried to implement code replacement

--- clang-tools-extra/clangd/refactor/tweaks/DoMagic.cpp
+++ clang-tools-extra/clangd/refactor/tweaks/DoMagic.cpp
@@ -10,6 +10,7 @@
 #include "refactor/Tweak.h"
 #include "support/Logger.h"
 #include "clang/AST/ASTContext.h"
+#include "clang/AST/ExprConcepts.h"
 #include "clang/AST/Stmt.h"
 #include "clang/Basic/LangOptions.h"
 #include "clang/Basic/SourceLocation.h"
@@ -33,16 +34,49 @@ public:
   llvm::StringLiteral kind() const override {
     return CodeAction::REFACTOR_KIND;
   }
+
+private:
+  const ConceptSpecializationExpr *ConceptSpecializationExpression = nullptr;
 };
 
 REGISTER_TWEAK(DoMagic)
 
 bool DoMagic::prepare(const Selection &Inputs) {
+  const auto *Node = Inputs.ASTSelection.commonAncestor()->Parent;
+  const auto *Expression = Node->ASTNode.get<Expr>();
+
+  ConceptSpecializationExpression = dyn_cast_or_null<ConceptSpecializationExpr>(Expression);
+
+  if(ConceptSpecializationExpression == nullptr)
+  {
+    return false;
+  }
+
   return true;
 }
 
 Expected<Tweak::Effect> DoMagic::apply(const Selection &Inputs) {
-  return Effect::showMessage("Magic happened!! \\(*0*)/");
+  auto &Ctx = Inputs.AST->getASTContext();
+  auto &SrcMgr = Inputs.AST->getSourceManager();
+
+  auto TypeRng = toHalfOpenFileRange(SrcMgr, Ctx.getLangOpts(),
+                      ConceptSpecializationExpression->getSourceRange());
+
+  if(!TypeRng)
+    return error("Could not obtain range of the 'type' branch. Macros?");
+
+  auto TypeCode = toSourceCode(SrcMgr, *TypeRng);
+
+  tooling::Replacements Result;
+  if(auto Err = Result.add(tooling::Replacement(Ctx.getSourceManager(),
+                                  TypeRng->getBegin(),
+                                  TypeCode.size(),
+                                  TypeCode)))
+    return std::move(Err);
+
+  return Effect::mainFileEdit(SrcMgr, std::move(Result));
+
+  //return Effect::showMessage("I'm a concept! Magic happened!! \\(*0*)/");
 }
 
 } // namespace
