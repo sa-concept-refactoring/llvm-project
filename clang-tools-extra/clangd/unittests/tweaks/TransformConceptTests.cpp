5e2da6626503513e864cd4ae1f470f4c20c1e5cd
Jeremy Stucki <dev@jeremystucki.ch>
2023-10-09 16:01:20 +0200

    Work on tests

--- clang-tools-extra/clangd/unittests/tweaks/TransformConceptTests.cpp
+++ clang-tools-extra/clangd/unittests/tweaks/TransformConceptTests.cpp
@@ -18,9 +18,49 @@ namespace {
 TWEAK_TEST(TransformConcept);
 
 TEST_F(TransformConceptTest, Test) {
-  Context = Function;
-  EXPECT_EQ(apply("^template<typename T> void f(T) requires std::integral<T> {}"),
-            "template<std::integral T> void f(T) {}");
+//  EXPECT_EQ(apply("template<typename T> void f(T) requires ^std::integral<T> {}"), "template<std::integral T> void f(T) {}");
+
+  Header =
+    R"cpp(
+      template <typename T>
+      concept foo = true;
+
+      template <typename T>
+      concept bar = true;
+
+      template <typename T, typename U>
+      concept baz = true;
+    )cpp";
+
+  ExtraArgs = { "-std=c++20" };
+
+  EXPECT_AVAILABLE(
+    R"cpp(
+      template <typename T> void f(T)
+        requires ^f^o^o^<^T^> {}
+    )cpp"
+  );
+
+  EXPECT_AVAILABLE(
+      R"cpp(
+      template <typename T, typename U> void f(T)
+        requires ^f^o^o^<^T^> {}
+    )cpp"
+  );
+
+  EXPECT_UNAVAILABLE(
+    R"cpp(
+      template <bar T> void f(T)
+        requires ^f^o^o^<^T^> {}
+    )cpp"
+  );
+
+  EXPECT_UNAVAILABLE(
+    R"cpp(
+      template <typename T, typename U> void f(T, U)
+        requires ^b^a^z^<^T^,^ ^U^> {}
+    )cpp"
+  );
 }
 
 } // namespace
